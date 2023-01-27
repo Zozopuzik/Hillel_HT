cee3ae3ce93f3dc653d840c6e17c4d4856e5ebb6
zozopuzik <evgen.sabadash1337@gmail.com>
2023-01-27 22:36:40 +0200

    home task complete

--- task_2/index.js
+++ task_2/index.js
@@ -1,1 +1,6 @@
-let a = 1
+let birthYear   = +prompt("What is your birth year?")
+let name        = prompt("What is your name?")
+let surname     = prompt("What is your surname?")
+const nowYear   = 2023
+
+alert(`User Bio: ${name} ${surname}, ${nowYear - birthYear} years old;`)
