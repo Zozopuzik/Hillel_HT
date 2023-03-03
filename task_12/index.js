dfbcfd8f4ffbde3383246ac15b1a72f7216fd4ad
zozopuzik <evgen.sabadash1337@gmail.com>
2023-03-04 00:59:31 +0200

    code formatted and looks amazing

--- task_12/index.js
+++ task_12/index.js
@@ -1,49 +1,48 @@
 // Setting variables
-let size = '';
-let stuffing = '';
+let size = "";
+let stuffing = "";
 let hamburger;
 class Hamburger {
   // Sizes
   static SIZE_SMALL = {
-    size: 'small',
+    size: "small",
     price: 50,
-    calories: 20
-  };
-  
-  static SIZE_BIG = {
-    size: 'big',
-    price: 100,
-    calories: 40
-  };
-  
-  // Stuffing
-  static STUFFING_CHEESE = {
-    stuffing: 'cheese',
-    price: 10,
-    calories: 20
-  };
-  
-  static STUFFING_SALAD = {
-    stuffing: 'salad',
-    price: 20,
-    calories: 5
-  };
-  static STUFFING_POTATO = {
-    stuffing: 'potato',
-    price: 15,
-    calories: 10
+    calories: 20,
   };
 
-  
+  static SIZE_BIG = {
+    size: "big",
+    price: 100,
+    calories: 40,
+  };
+
+  // Stuffing
+  static STUFFING_CHEESE = {
+    stuffing: "cheese",
+    price: 10,
+    calories: 20,
+  };
+
+  static STUFFING_SALAD = {
+    stuffing: "salad",
+    price: 20,
+    calories: 5,
+  };
+  static STUFFING_POTATO = {
+    stuffing: "potato",
+    price: 15,
+    calories: 10,
+  };
+
   // Toppings
   static TOPPING_MAYO = {
     price: 20,
-    calories: 5
+    calories: 5,
   };
-  
+
   static TOPPING_SAUSE = {
     price: 15,
-    calories: 0
+    calories: 0,
   };
 
   constructor(objSize, objFeeling) {
@@ -92,35 +91,32 @@ const createBurger = () => {
   let userStuffing;
   const price = document.getElementById("price");
   const calories = document.getElementById("calories");
-  size === "small" ? (userSize = Hamburger.SIZE_SMALL) : (userSize = Hamburger.SIZE_BIG);
-    if(stuffing === "cheese"){
-    userStuffing = Hamburger.STUFFING_CHEESE
+  size === "small"
+    ? (userSize = Hamburger.SIZE_SMALL)
+    : (userSize = Hamburger.SIZE_BIG);
+  if (stuffing === "cheese") {
+    userStuffing = Hamburger.STUFFING_CHEESE;
+  } else if (stuffing === "salad") {
+    userStuffing = Hamburger.STUFFING_SALAD;
+  } else if (stuffing === "potato") {
+    userStuffing = Hamburger.STUFFING_POTATO;
   }
-    
-    else if(stuffing === "salad"){
-        userStuffing = Hamburger.STUFFING_SALAD
-    }
-    else if(stuffing === 'potato'){
-        userStuffing = Hamburger.STUFFING_POTATO
-    } 
 
-    hamburger = new Hamburger(userSize, userStuffing);
+  hamburger = new Hamburger(userSize, userStuffing);
 
   price.innerHTML = hamburger.calculatePrice();
   calories.innerHTML = hamburger.calculateCalories();
 };
 
-
-const addToping = document.getElementsByClassName('addToping');
+const addToping = document.getElementsByClassName("addToping");
 for (let i = 0; i < addToping.length; i++) {
-    addToping[i].addEventListener("click", function() {
-      if(addToping[i].value === 'Add Mayo'){
-        hamburger.addTopping(Hamburger.TOPPING_MAYO)
-        price.innerHTML = hamburger.calculatePrice();
-        calories.innerHTML = hamburger.calculateCalories();
-      }
-      else(hamburger.addTopping(Hamburger.TOPPING_SAUSE))
+  addToping[i].addEventListener("click", function () {
+    if (addToping[i].value === "Add Mayo") {
+      hamburger.addTopping(Hamburger.TOPPING_MAYO);
       price.innerHTML = hamburger.calculatePrice();
       calories.innerHTML = hamburger.calculateCalories();
-    });
-  }
+    } else hamburger.addTopping(Hamburger.TOPPING_SAUSE);
+    price.innerHTML = hamburger.calculatePrice();
+    calories.innerHTML = hamburger.calculateCalories();
+  });
+}
